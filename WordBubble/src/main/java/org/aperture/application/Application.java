ccee63810357506bfcd073cd02394e80074af1a1
adams52 <adams.b52@gmail.com>
2016-06-15 08:24:21 -0500

    Committing the initial project structure
    Will need to change a bunch of stuff after this, but this will let Nive
    have some stuff

---
+++ WordBubble/src/main/java/org/aperture/application/Application.java
@@ -0,0 +1,18 @@
+package org.aperture.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.*;
+
+@EnableAutoConfiguration
+@ComponentScan({"org.aperture.controller"})
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
