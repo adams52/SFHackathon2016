a1ab8d4a9a9e8bfc98565bacaf29da9068ae8ac7
nivesuresh <nivesuresh91@gmail.com>
2016-06-15 14:04:37 -0500

    Made changes to db and made the counts smaller so that it works with the api

--- WordBubble/src/main/java/org/aperture/data/WordBubbleSQL.java
+++ WordBubble/src/main/java/org/aperture/data/WordBubbleSQL.java
@@ -6,36 +6,36 @@ public class WordBubbleSQL {
 			"CREATE TABLE BusinessSocial (Name string, Community string, Word string, Count integer)";
 	
 	public static final String INSERT_DATA = "INSERT INTO BusinessSocial (Name, Community, Word, Count) " +
-			"VALUES ('Ben Adams', 'Red Mic', 'Red', 10)" +
-			", ('Ben Adams', 'Red Mic', 'Red', 15)" +
-			", ('Ben Adams', 'Red Mic', 'Mic', 25)" +
-			", ('Ben Adams', 'Systems Technology', 'Autonomous', 30)" +
-			", ('Ben Adams', 'Systems Technology', 'Vehicles', 43)" +
-			", ('Ben Adams', 'Systems Technology', 'Bridge', 22)" +
-			", ('Ben Adams', 'Ask Oak', 'Star', 7)" +
+			"VALUES ('Ben Adams', 'Red Mic', 'Red', 2)" +
+			", ('Ben Adams', 'Red Mic', 'Red', 3)" +
+			", ('Ben Adams', 'Red Mic', 'Mic', 5)" +
+			", ('Ben Adams', 'Systems Technology', 'Autonomous', 3)" +
+			", ('Ben Adams', 'Systems Technology', 'Vehicles', 4)" +
+			", ('Ben Adams', 'Systems Technology', 'Bridge', 2)" +
+			", ('Ben Adams', 'Ask Oak', 'Star', 3)" +
 			", ('Ben Adams', 'Ask Oak', 'IT', 5)" +
-			", ('Ben Adams', 'Ask Oak', 'Modernization', 32)" +
-			", ('Ben Adams', 'Ask Oak', 'Remarkable', 25)" +
-			", ('Nive Suresh', 'Red Mic', 'IT', 27)" +
-			", ('Nive Suresh', 'Red Mic', 'Modernization', 37)" +
-			", ('Nive Suresh', 'Red Mic', 'Remarkable', 41)" +
-			", ('Nive Suresh', 'Red Mic', 'Service', 30)" +
-			", ('Nive Suresh', 'Systems Technology', 'Autonomous', 40)" +
-			", ('Nive Suresh', 'Systems Technology', 'Cars', 47)" +
+			", ('Ben Adams', 'Ask Oak', 'Modernization', 4)" +
+			", ('Ben Adams', 'Ask Oak', 'Remarkable', 5)" +
+			", ('Nive Suresh', 'Red Mic', 'IT', 7)" +
+			", ('Nive Suresh', 'Red Mic', 'Modernization', 3)" +
+			", ('Nive Suresh', 'Red Mic', 'Remarkable', 1)" +
+			", ('Nive Suresh', 'Red Mic', 'Service', 8)" +
+			", ('Nive Suresh', 'Systems Technology', 'Autonomous', 2)" +
+			", ('Nive Suresh', 'Systems Technology', 'Cars', 5)" +
 			", ('Nive Suresh', 'Systems Technology', 'Hackday', 13)" +
-			", ('Nive Suresh', 'Ask Oak', 'Mark', 20)" +
-			", ('Nive Suresh', 'Ask Oak', 'Oakley', 32)" +
-			", ('Nive Suresh', 'Ask Oak', 'Laptop', 17)" +
-			", ('Joanna Snipes', 'Red Mic', 'IT', 19)" +
-			", ('Joanna Snipes', 'Red Mic', 'Red', 12)" + 
-			", ('Joanna Snipes', 'Red Mic', 'Ask', 15)" + 
-			", ('Joanna Snipes', 'Systems Technology', 'Modernization', 20)" + 
-			", ('Joanna Snipes', 'Systems Technology', 'Android', 20)" + 
-			", ('Joanna Snipes', 'Systems Technology', 'Bands', 12)" + 
+			", ('Nive Suresh', 'Ask Oak', 'Mark', 2)" +
+			", ('Nive Suresh', 'Ask Oak', 'Oakley', 2)" +
+			", ('Nive Suresh', 'Ask Oak', 'Laptop', 7)" +
+			", ('Joanna Snipes', 'Red Mic', 'IT', 1)" +
+			", ('Joanna Snipes', 'Red Mic', 'Red', 2)" + 
+			", ('Joanna Snipes', 'Red Mic', 'Ask', 5)" + 
+			", ('Joanna Snipes', 'Systems Technology', 'Modernization', 2)" + 
+			", ('Joanna Snipes', 'Systems Technology', 'Android', 9)" + 
+			", ('Joanna Snipes', 'Systems Technology', 'Bands', 2)" + 
 			", ('Joanna Snipes', 'Systems Technology', 'Autonomous', 10)" + 
-			", ('Joanna Snipes', 'Ask Oak', 'Hacking', 22)" + 
-			", ('Joanna Snipes', 'Ask Oak', 'Production', 13)" + 
-			", ('Joanna Snipes', 'Ask Oak', 'Test', 15)";
+			", ('Joanna Snipes', 'Ask Oak', 'Hacking', 7)" + 
+			", ('Joanna Snipes', 'Ask Oak', 'Production', 3)" + 
+			", ('Joanna Snipes', 'Ask Oak', 'Test', 5)";
 	
 	public static final String SELECT_ALL_DATA = "SELECT * FROM BusinessSocial";
 	
